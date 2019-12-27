187069faa427b60bc289b7f259cf750372103a99
silvodesigns <kevsilvo@gmail.com>
2019-12-27 16:22:32 -0500

    added ListContacts.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>This is my react app</h1>
-    </div>
-  );
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+]
+
+class App extends Component {
+ render() {
+      return (
+          <div className="App">
+          <h1>This is my react app</h1>
+          </div>
+      );
+ }
 }
 
 export default App;
---
+++ src/ListContacts.js
@@ -0,0 +1,11 @@
+import React, {component} from 'react';
+
+class ListContacts extends Component {
+    render(){
+        return (
+            <ol className="contact-list"></ol>
+        )
+    }
+}
+
+export default ListContacts;
