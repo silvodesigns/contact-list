cb93e21f24a2028cd06e9026a580b48dbec56d3a
silvodesigns <kevsilvo@gmail.com>
2020-01-02 20:17:14 -0500

    added controlled component input

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,23 +1,47 @@
 import React from 'react';
 import PropTypes from  'prop-types';
 
-function ListContacts(props){
+class ListContacts extends React.Component{
+
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState({query})
+
+    }
+
+    render(){
         return (
-            <ol className="contact-list">
-                {props.contacts.map((contact) => (
+        <div className="list-contacts">
+            <div className="list-contacts-top">
+                <input
+                 className='search-contacts'
+                 type='text'
+                 placeholder='search contacts'
+                 value={this.state.query} 
+                 onChange={(event) => this.updateQuery(event.target.value)}/>
+
+            </div>
+             <ol className="contact-list">
+                {this.props.contacts.map((contact) => (
                 <li key={contact.id} className='contact-list-item'>
                    <div className='contact-avatar' style={{backgroundImage: `url(${contact.avatarURL})`}} />
                         <div className='contact-details'>
                             <p>{contact.name}</p>
                             <p>{contact.email}</p>
                         </div>
-                        <a href="#"  onClick={()=> props.onDeleteContact(contact)} className='contact-remove'>Remove</a>
-                  
+                        <a href="#"  onClick={()=> this.props.onDeleteContact(contact)} className='contact-remove'>Remove</a>
                 </li>
                 ))}
             </ol>
+        </div>
+           
         )
+    }
 }
+
 
 
 ListContacts.propTypes = {
